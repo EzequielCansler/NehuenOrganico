469782d599aa1b82b5623fec8c419ebba9ede69a
Ezequiel <ezequielcansler91@gmail.com>
2024-08-05 19:29:26 -0300

    SQLite

--- Program.cs
+++ Program.cs
@@ -50,13 +50,6 @@ app.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}"
     );
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
 
 app.UseHttpsRedirection();
 
